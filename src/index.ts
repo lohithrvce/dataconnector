4fade31e5c4efde8b405b339ef06f0d6bb660faa
Lohith R <lohith.r@tredence.com>
2022-12-29 14:27:44 +0530

    data connector

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette } from '@jupyterlab/apputils';
+
 /**
  * Initialization data for the dataconnector extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'dataconnector:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension dataconnector is activated!');
+    console.log('ICommandPalette:', palette);
   }
 };
 
